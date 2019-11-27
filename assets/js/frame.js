ebee149023786ec6d8485eb9efe62fcd6f8bd9b6
Ricardo Caballero <ricardo.caballero.1996@gmail.com>
2019-11-27 22:28:37 +0100

    Cambios en vista de diseño + enlace de botones header

--- assets/js/frame.js
+++ assets/js/frame.js
@@ -1,3 +1,12 @@
+
+let pathFrame = path.join(__dirname,'../assets/js/classFrame.js')
+const classFrame = require(pathFrame)
+
+$(() => {
+  $('.editEnabled').hide();
+});
+
+
 /* The dragging code for '.draggable' from the demo above
  * applies to this demo as well so it doesn't have to be repeated. */
 // target elements with the "draggable" class
@@ -136,5 +145,92 @@ interact('.dropzone').dropzone({
     event.target.style.transform =
         'translate(' + x + 'px, ' + y + 'px)'
   })*/
+  
+
+  ipc.on('frameInf', function(event, arg){
+    $('#frames').append('<option value= "frame'+nFrames+'">Frame'+nFrames+'</option>');
+    //<a id= "frame'+nFrames+'" onclick="seleccionarFrame("frame'+nFrames+'")">Frame'+nFrames+' </a>
+    nFrames++;
+    let framePrueba = new classFrame.Frame("0","caca","rojo");
+  })
+
+  function createNewVar(){
+    winnewFrame=new BrowserWindow({ 
+      width: 400,
+      height: 330,
+      webPreferences: {
+        nodeIntegration: true,
+        defaultEncoding: 'UTF-8'
+      }
+    });
+    winnewFrame.loadURL(`file://${__dirname}/newVar.html`);
+    winnewFrame.on('close',function(){winnewFrame=null});
+    //winnewFrame.show();
+    winnewFrame.once('ready-to-show', () => {
+      winnewFrame.setMenu(null)
+      winnewFrame.show()
+    })
+  }
+  const vars= document.getElementById('vars');
+  const varsMov=document.getElementById('varsMov');
+
+  ipc.on('varInf', function(event, arg){
+    //vars.innerHTML +='<a id="variable'+nVars+'" class="btn btn-info btn-lg">Variable'+nVars+'</a>';
+    vars.innerHTML += '<div class="card border-info mb-3 text-center">\
+                              <div class="card-header">\
+                                <a id="variable'+nVars+' class="collapsed card-link text-center" data-toggle="collapse" href="#collapse'+nVars+'">\
+                                <h6 class="card-title text-dark"> <i class="far fa-edit"></i> Variable'+nVars+'</h6>\
+                              </a>\
+                              </div>\
+                              <div id="collapse'+nVars+'" class="collapse" data-parent="#accordion">\
+                                <div class="card-body text-left">\
+                                <table class="table table-hover group table-striped">\
+                                  <tbody>\
+                                    <tr>\
+                                    <td>Nombre:</td>\
+                                    <td>Value</td>\
+                                   </tr>\
+                                    <tr>\
+                                    <td>Tipo:</td>\
+                                    <td>Value</td>\
+                                   </tr>\
+                                    <tr>\
+                                    <td>Formato:</td>\
+                                    <td>Value</td>\
+                                   </tr>\
+                                   <tr>\
+                                    <td>Etiqueta:</td>\
+                                    <td>Value</td>\
+                                    </tr>\
+                                    <tr>\
+                                    <td>Valor inicial:</td>\
+                                    <td>Value</td>\
+                                    </tr>\
+                                  </tbody>\
+                                </table>\
+                              </div> </div> </div>';
+    $('#collapse'+nVars).append('<div class="card-footer text-muted">\
+                                  <a href="#" class="btn btn-sm btn-info"><i class="far fa-save"></i> Guardar Cambios </a>\
+                                </div>');
+    $('#collapse'+nVars).append('<div class="card-footer text-muted">\
+                                  <a href="#" class="btn btn-sm btn-info"><i class="fas fa-trash-alt"></i> Borrar </a>\
+                                </div>');  
+    varsMov.innerHTML +='<div id="yes-drop" class="drag-drop"> variable'+ nVars +'</div>';
+    nVars++;
+  })
+  //AL CAMBIAR DE FRAME CARGAR VISTA DE NUEVO FRAME CON SUS VARIABLES
+  $(document).on('change', '#frames', function(event) {
+    let id = $("#frames option:selected").text();
+    if($("#frames option:selected").attr('value') == 0){
+      $('.editEnabled').hide();
+    }
+    else{
+      $('.frameSelected').empty();
+      $('.frameSelected').append('<label>'+id+'</label>');
+      $('.frameSelected').append('<label> Editable futuramente</label>');
+    $('.editEnabled').show();
+    }
+    
+});
 
     
--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -8,9 +8,6 @@ window.frames["../../src/frame.html"]
 
 const electron = require('electron')
 const path = require('path')
-let pathFrame = path.join(__dirname,'../assets/js/classFrame.js')
-console.log(pathFrame)
-const Frame = require(pathFrame)
 const BrowserWindow = electron.remote.BrowserWindow
 const remote=electron.remote
 const ipc=electron.ipcRenderer
@@ -29,6 +26,10 @@ newFrame.addEventListener('click', () => {
     createNewFrame();
   });*/
 
+$(() => {
+   $(".headerOptions").hide();
+   $('#designer').hide();
+});
   
 function createNewFrame(){
     winnewFrame=new BrowserWindow({ 
@@ -48,81 +49,9 @@ function createNewFrame(){
     })
 }
 
-  const frames= document.getElementById('frames');
-
-  ipc.on('frameInf', function(event, arg){
-    frames.innerHTML +='<a id="frame'+nFrames+'" class="btn btn-info btn-lg">Frame'+nFrames+'</a>';
-    nFrames++;
-    //Crear clase frame
-    console.log("Antes de crear frame");
-    let framePrueba = new Frame("0","caca","rojo");
-    if(framePrueba != null){
-      alert("Creado frame");
-    }
-  })
-
-  function createNewVar(){
-    winnewFrame=new BrowserWindow({ 
-      width: 400,
-      height: 330,
-      webPreferences: {
-        nodeIntegration: true,
-        defaultEncoding: 'UTF-8'
-      }
-    });
-    winnewFrame.loadURL(`file://${__dirname}/newVar.html`);
-    winnewFrame.on('close',function(){winnewFrame=null});
-    //winnewFrame.show();
-    winnewFrame.once('ready-to-show', () => {
-      winnewFrame.setMenu(null)
-      winnewFrame.show()
-    })
+  function crearPlantilla(){
+    $('#index').hide();
+    $('#designer').show();
+    $('.headerOptions').show();
   }
-  const vars= document.getElementById('vars');
-  const varsMov=document.getElementById('varsMov');
-
-  ipc.on('varInf', function(event, arg){
-    //vars.innerHTML +='<a id="variable'+nVars+'" class="btn btn-info btn-lg">Variable'+nVars+'</a>';
-    vars.innerHTML += '<div class="card border-info mb-3 text-center">\
-                              <div class="card-header">\
-                                <a id="variable'+nVars+' class="collapsed card-link text-center" data-toggle="collapse" href="#collapse'+nVars+'">\
-                                <h6 class="card-title text-dark"> <i class="far fa-edit"></i> Variable'+nVars+'</h6>\
-                              </a>\
-                              </div>\
-                              <div id="collapse'+nVars+'" class="collapse" data-parent="#accordion">\
-                                <div class="card-body text-left">\
-                                <table class="table table-hover group table-striped">\
-                                  <tbody>\
-                                    <tr>\
-                                    <td>Nombre:</td>\
-                                    <td>Value</td>\
-                                   </tr>\
-                                    <tr>\
-                                    <td>Tipo:</td>\
-                                    <td>Value</td>\
-                                   </tr>\
-                                    <tr>\
-                                    <td>Formato:</td>\
-                                    <td>Value</td>\
-                                   </tr>\
-                                   <tr>\
-                                    <td>Etiqueta:</td>\
-                                    <td>Value</td>\
-                                    </tr>\
-                                    <tr>\
-                                    <td>Valor inicial:</td>\
-                                    <td>Value</td>\
-                                    </tr>\
-                                  </tbody>\
-                                </table>\
-                              </div> </div> </div>';
-    $('#collapse'+nVars).append('<div class="card-footer text-muted">\
-                                  <a href="#" class="btn btn-sm btn-info"><i class="far fa-save"></i> Guardar Cambios </a>\
-                                </div>');
-    $('#collapse'+nVars).append('<div class="card-footer text-muted">\
-                                  <a href="#" class="btn btn-sm btn-info"><i class="fas fa-trash-alt"></i> Borrar </a>\
-                                </div>');  
-    varsMov.innerHTML +='<div id="yes-drop" class="drag-drop"> variable'+ nVars +'</div>';
-    nVars++;
-  })
   
