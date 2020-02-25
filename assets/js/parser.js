7ab2e08b5e6c9a601ed49356db153e7f4be507d4
Ricardo Caballero <ricardo.caballero.1996@gmail.com>
2020-02-25 21:31:27 +0100

    pegjs creado

--- assets/js/parser.js
+++ assets/js/parser.js
@@ -1,8 +1,10 @@
 
-var peg = require("pegjs");
+var parser = require(path.join(__dirname,'../js/prueba.js'));
 
-var parser = peg.generate("start = ('a' / 'b')+");
+// var parser = peg.generate("start = ('a' / 'b')+");
 exports.pruebaParseo = function pruebaParseo(){
+    console.log(parser.parse("2*(3+4)"));
     console.log(parser.parse("abba"));
     console.log(parser.parse("abca"));
+    
 }
